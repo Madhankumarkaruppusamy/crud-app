383ebee782b84ae4a2d8f266f7903e06432537f8
madhankumarkaruppusamy <madhankumarkaruppusamy@gmail.com>
2024-05-30 14:25:16 +0530

    Completed

--- src/app/emp-add-edit/emp-add-edit.component.ts
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,37 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, Inject } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { EmployeeService } from '../services/employee.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CoreService } from '../core/core.service';
 
 @Component({
   selector: 'app-emp-add-edit',
   templateUrl: './emp-add-edit.component.html',
   styleUrls: ['./emp-add-edit.component.scss']
 })
-export class EmpAddEditComponent {
-  empform:FormGroup;
+export class EmpAddEditComponent implements OnInit {
+  empform: FormGroup;
 
-  education:string[]=[
+  education: string[] = [
     'Matric',
     'Diploma',
     'Intermediate',
     'Graduate',
     'Post Graduate'
   ]
-  constructor(private _fb:FormBuilder){
-    this.empform=this._fb.group({
-        firstName: '',
-        lastName: '',
-        email: '',
-        dob: '',
-        gender: '',
-        education: '',
-        company: '',
-        experience:'',
-        package: ''
+  constructor(
+    private _fb: FormBuilder,
+    private _empService: EmployeeService,
+    private _dialogRef: MatDialogRef<EmpAddEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _coreService:CoreService
+  ) {
+
+
+    this.empform = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      company: '',
+      experience: '',
+      package: ''
     })
   }
-  OnFormSubmit(){
-    if(this.empform.valid){
-      console.log(this.empform.value);
+
+  ngOnInit(): void {
+    this.empform.patchValue(this.data);
+  }
+
+  OnFormSubmit() {
+    if (this.empform.valid) {
+      if (this.data) {
+        this._empService.updateEmployee(this.data.id, this.empform.value).subscribe({
+          next: (val: any) => {
+            this._coreService.openSnackBar('Employee updated!','')
+            this._dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err);
+          }
+        })
+
+      }
+      else {
+        this._empService.addEmployee(this.empform.value).subscribe({
+          next: (val: any) => {
+            this._coreService.openSnackBar('Employee saved!','')
+            this._dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err);
+          }
+        })
+      }
     }
   }
 }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -7,10 +7,22 @@ import { Observable } from 'rxjs';
 })
 export class EmployeeService {
 
-  constructor(private _http:HttpClient) {}
+  constructor(private _http: HttpClient) { }
 
-    addEmployee(data:any):Observable<any>{
-      return this._http.post("http://localhost:3000/employee",data);
-    
-   }
+  addEmployee(data: any) {
+    return this._http.post<any>("http://localhost:3000/employee", data);
+
+  }
+  getEmployeeList(): Observable<any> {
+    return this._http.get("http://localhost:3000/employee");
+
+  }
+  updateEmployee(id:number,data: any) {
+    return this._http.put<any>(`http://localhost:3000/employee/${id}`,data);
+
+  }
+  deleteEmployee(id: number): Observable<any> {
+    return this._http.delete(`http://localhost:3000/employee/${id}`);
+
+  }
 }
